3d08434a092a714ae721712fa62a6884e9933d30
Julian <julikq22@gmail.com>
2023-09-26 17:33:02 +0200

    Commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,6 @@
 Cypress.Commands.add('aceptarCookies', () => {
-    cy.get('.jw8mI', { failOnStatusCode: false }).then(($element) => {
-        if ($element.length > 0) {
+    cy.find('.jw8mI', { failOnStatusCode: false }).then(($element) => {
+        if ($element.length) {
         cy.get('.jw8mI').should('contain.text','Antes de ir a Google') 
         } else {
 
