0100b391224f48798676f3764535b42637c445bd
Julian <julikq22@gmail.com>
2023-09-26 16:51:40 +0200

    Se ha creando un Commands para que en caso que aparezca el popup de cookies, se acepten y siga la prueba

--- cypress/e2e/tests/prueba.cy.js
+++ cypress/e2e/tests/prueba.cy.js
@@ -6,7 +6,7 @@ removeLogs();
 describe('Automatización de una web', () => {
     beforeEach('Buscar en Google el criterio Automatización', () => {
         cy.visit('https://www.google.com/');
-        google.cookies();
+        cy.aceptarCookies();
         google.get.buscador().type('Automatización {enter}');
         cy.scrollTo(0,500);
         cy.get('h3').contains('Automatización - Wikipedia, la enciclopedia libre').click({force:true});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,12 +1,9 @@
-Cypress.Commands.add('goToPage', () =>{
-    cy.visit('http://petstore.swagger.io/v2/user', {
-        failOnStatusCode: false,
-        headers: {
-            "Connection": "keep-alive",
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
-            "accept": "/",
-            "accept-encoding": "gzip, deflate",
-            "referer": "http://petstore.swagger.io/v2/user"
-        },
-    });
+Cypress.Commands.add('aceptarCookies', () => {
+    cy.get('.jw8mI', { timeout: 5000 })
+    .should('be.visible') 
+    .then(($popup) => {
+        if ($popup.find('#L2AGLb > .QS5gu').length > 0) {
+        cy.get('#L2AGLb > .QS5gu').click(); 
+        }
+    })
 });
